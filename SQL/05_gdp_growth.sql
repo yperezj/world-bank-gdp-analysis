9f4d97fcf4b2a945ef224160f03d6de19827cbf3
yperezj <perezjimenezylenia@gmail.com>
2026-08-19 20:25:39 +0200

    Update 05_gdp_growth.sql

--- SQL/05_gdp_growth.sql
+++ SQL/05_gdp_growth.sql
@@ -1,23 +1,29 @@
+/*Calculate annual GDP growth rates and growth indexes.*/
+
 CREATE VIEW growth AS(
+  
+  WITH year_comparison AS(
+    
+    SELECT 
+      c.iso_code AS iso_code,
+      i.code AS indicator_code,
+      year,
+      value,
+      LAG(year) OVER(PARTITION BY country_id, indicator_id ORDER BY year) AS last_year,
+      LAG(value) OVER(PARTITION BY country_id, indicator_id ORDER BY year) AS last_value
 
-WITH year_comparison AS(
+    FROM 
+      observations o
+      JOIN countries c ON o.country_id=c.id
+      JOIN indicators i ON o.indicator_id=i.id)
 
 SELECT 
-c.iso_code AS iso_code,
-i.code AS indicator_code,
-year,
-value,
-LAG(year) OVER(PARTITION BY country_id, indicator_id ORDER BY year) AS last_year,
-LAG(value) OVER(PARTITION BY country_id, indicator_id ORDER BY year) AS last_value
-
-FROM observations o
-JOIN countries c ON o.country_id=c.id
-JOIN indicators i ON o.indicator_id=i.id)
-
-SELECT 
-iso_code, indicator_code,year, value, last_value,
-ROUND(100.00*value/last_value,2) AS growth_index,
-ROUND(100.00*(value-last_value)/last_value,2) AS growth_rate
+  iso_code, 
+  indicator_code,year, 
+  value, 
+  last_value,
+  ROUND(100.00*value/last_value,2) AS growth_index,
+  ROUND(100.00*(value-last_value)/last_value,2) AS growth_rate
 
 FROM year_comparison
 
