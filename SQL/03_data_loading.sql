f17c279b7401c800d5d0d94043d5aa3904c0ef6d
yperezj <perezjimenezylenia@gmail.com>
2026-08-19 19:55:56 +0200

    Update 03_data_loading.sql

--- SQL/03_data_loading.sql
+++ SQL/03_data_loading.sql
@@ -3,28 +3,30 @@
 INSERT INTO countries (name,iso_code)
 
 SELECT 
-DISTINCT "Country Name",
-"Country Code"
-
-FROM pib
-
-;
-
-
-
-INSERT INTO indicators (indicator, code)
-
-SELECT DISTINCT "Indicator Name",
-"Indicator Code"
+  DISTINCT "Country Name",
+  "Country Code"
 
 FROM pib;
 
 
+INSERT INTO indicators (indicator, code)
+
+SELECT 
+  DISTINCT "Indicator Name",
+  "Indicator Code"
+
+FROM pib;
+
 
 INSERT INTO observations (country_id, indicator_id, year, value)
 
-SELECT c.id, i.id, n.year, n.value 
+SELECT 
+  c.id, 
+  i.id, 
+  n.year, 
+  n.value 
 
-FROM unpivot_pib n 
-INNER JOIN countries c ON n.country=c.name
-INNER JOIN indicators i ON n.ind=i.indicator;
+FROM 
+  unpivot_pib n 
+  INNER JOIN countries c ON n.country=c.name
+  INNER JOIN indicators i ON n.ind=i.indicator;
