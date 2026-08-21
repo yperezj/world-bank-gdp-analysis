c5b7fc25ef8b3771664d762b6d0052df320477dd
yperezj <perezjimenezylenia@gmail.com>
2026-08-21 22:49:48 +0200

    Update 09_spain_queries.sql

--- SQL/09_spain_queries.sql
+++ SQL/09_spain_queries.sql
@@ -4,36 +4,39 @@ SELECT *
 
 FROM growth 
 
-WHERE iso_code='ESP';
+WHERE 
+  iso_code='ESP'
+  AND indicator_code='NY.GDP.MKTP.CD';
 
 /*Spain cumulatve CAGR*/
 
 SELECT 
+  g.year, 
+ g.value,
+ g.growth_index,
+ g.growth_rate,
+ c.first_year, 
+ c.cagr
 
-g.year, 
-g.value,
-g.growth_index,
-g.growth_rate,
-c.first_year, 
-c.cagr
+FROM 
+  growth g 
+  JOIN cagr c ON g.iso_code=c.iso_code 
+  AND g.year=c.year 
+  AND g.indicator_code=c.indicator_code
 
-FROM growth g 
-JOIN cagr c ON g.iso_code=c.iso_code 
-AND g.year=c.year 
-AND g.indicator_code=c.indicator_code
-
-WHERE g.iso_code='ESP' 
-AND g.indicator_code='NY.GDP.MKTP.CD';
+WHERE 
+  g.iso_code='ESP' 
+  AND g.indicator_code='NY.GDP.MKTP.CD';
 
 /*Spain fixed ten-year CAGR*/
 
 SELECT 
-first_year || '-' || year AS period,
-first_value AS initial_value,
-value AS final_value,
-cagr
+  first_year || '-' || year AS period,
+  first_value AS initial_value,
+  value AS final_value,
+  cagr
 
-FROM ten_year_CAGR 
+FROM ten_year_cagr 
 
 WHERE iso_code='ESP'
 
