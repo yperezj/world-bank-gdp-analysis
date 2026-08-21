5ec3d13f98203878d67c8e836f47362001f42244
yperezj <perezjimenezylenia@gmail.com>
2026-08-21 22:41:14 +0200

    Update 07_fixed_10-year-cagr.sql

--- SQL/07_fixed_10-year-cagr.sql
+++ SQL/07_fixed_10-year-cagr.sql
@@ -1,32 +1,35 @@
+/*Calculate ten-year fixed period CAGR.*/
+
 CREATE VIEW ten_year_cagr AS(
+  
+  WITH decade_cagr AS(
 
-WITH decade_cagr AS(
+    SELECT
+      c.iso_code,
+      i.indicator,
+      o.year,
+      LAG(o.year) OVER(PARTITION BY c.iso_code, i.indicator ORDER BY year) AS first_year,
+      value,
+      LAG(o.value) OVER(PARTITION BY c.iso_code, i.indicator ORDER BY year) AS first_value
 
-SELECT
-c.iso_code,
-i.indicator,
-o.year,
-LAG(o.year) OVER(PARTITION BY c.iso_code, i.indicator ORDER BY year) AS first_year,
-value,
-LAG(o.value) OVER(PARTITION BY c.iso_code, i.indicator ORDER BY year) AS first_value
+    FROM 
+      observations o 
+      JOIN countries c ON o.country_id=c.id
+      JOIN indicators i ON o.indicator_id=i.id
 
-FROM observations o 
-JOIN countries c ON o.country_id=c.id
-JOIN indicators i ON o.indicator_id=i.id
+    WHERE o.year % 10=0)
 
-WHERE o.year%10=0
-
-)
-
-SELECT *,
-ROUND(POWER(1.00*value/first_value,1.00/10)-1,4) AS cagr
+  
+SELECT 
+  *,
+  ROUND(POWER(1.00 * value / first_value,1.00 / 10) - 1,4) AS cagr
 
 FROM decade_cagr 
 
-WHERE first_year IS NOT NULL 
-AND first_value IS NOT NULL 
-AND value IS NOT NULL
-
+WHERE 
+  first_year IS NOT NULL 
+  AND first_value IS NOT NULL 
+  AND value IS NOT NULL
 
 );
 
