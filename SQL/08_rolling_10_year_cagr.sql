aa93f2c319b34926ab73c67254ce6b953468f5fe
yperezj <perezjimenezylenia@gmail.com>
2026-08-21 22:45:24 +0200

    Update 08_rolling_10_year_cagr.sql

--- SQL/08_rolling_10_year_cagr.sql
+++ SQL/08_rolling_10_year_cagr.sql
@@ -23,12 +23,12 @@ SELECT
   s1.year|| '-' || s2.year AS period,
   s1.value AS initial_value,
   s2.value AS final_value,
-  ROUND((POWER(s2.value / s1.value,1.00 / 10)) - 1,4)
+  ROUND((POWER(s2.value / s1.value,1.00 / 10)) - 1,4) AS cagr
 
 
 FROM 
   summary s1 
-  JOIN summary s2 ON s1.year+10=s2.year 
+  JOIN summary s2 ON s1.year + 10=s2.year 
   AND s1.iso_code=s2.iso_code
   AND s1.indicator=s2.indicator
 
